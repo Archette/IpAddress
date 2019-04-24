5d7b04b1decf1eba1a3e7962025e70b5675a6014
Rixafy <rixafy@gmail.com>
2019-04-24 09:43:59 +0200

    Add IpAddressExtension

---
+++ src/IpAddressExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Archette\IpAddress;
+
+use Doctrine\Common\Persistence\Mapping\Driver\AnnotationDriver;
+use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\ServiceDefinition;
+use Rixafy\IpAddress\IpAddressFacade;
+use Rixafy\IpAddress\IpAddressFactory;
+use Rixafy\IpAddress\IpAddressRepository;
+use Rixafy\IpAddress\IpAddressResolver;
+
+class IpAddressExtension extends CompilerExtension
+{
+	public function beforeCompile()
+	{
+		/** @var ServiceDefinition $annotationDriver */
+		$annotationDriver = $this->getContainerBuilder()->getDefinitionByType(AnnotationDriver::class);
+		$annotationDriver->addSetup('addPaths', [['vendor/rixafy/ip-address']]);
+	}
+
+	public function loadConfiguration()
+	{
+		$this->getContainerBuilder()->addDefinition($this->prefix('ipAddressFactory'))
+			->setFactory(IpAddressFactory::class);
+
+		$this->getContainerBuilder()->addDefinition($this->prefix('ipAddressRepository'))
+			->setFactory(IpAddressRepository::class);
+
+		$this->getContainerBuilder()->addDefinition($this->prefix('ipAddressFacade'))
+			->setFactory(IpAddressFacade::class);
+
+		$this->getContainerBuilder()->addDefinition($this->prefix('ipAddressResolver'))
+			->setFactory(IpAddressResolver::class);
+	}
+}
