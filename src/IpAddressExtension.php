410d91c01e6e07439994053fd612a7345747e311
Rixafy <rixafy@gmail.com>
2020-06-21 07:34:43 +0200

    Add support for latest Nettrine

--- src/IpAddressExtension.php
+++ src/IpAddressExtension.php
@@ -16,9 +16,14 @@ class IpAddressExtension extends CompilerExtension
 {
 	public function beforeCompile(): void
 	{
-		/** @var ServiceDefinition $annotationDriver */
-		$annotationDriver = $this->getContainerBuilder()->getDefinitionByType(MappingDriver::class);
-		$annotationDriver->addSetup('addPaths', [['vendor/rixafy/ip-address']]);
+		if (class_exists('Nettrine\ORM\DI\Helpers\MappingHelper')) {
+			\Nettrine\ORM\DI\Helpers\MappingHelper::of($this)
+				->addAnnotation('Rixafy\IpAddress', __DIR__ . '/../../../rixafy/ip-address');
+		} else {
+			/** @var ServiceDefinition $annotationDriver */
+			$annotationDriver = $this->getContainerBuilder()->getDefinitionByType(MappingDriver::class);
+			$annotationDriver->addSetup('addPaths', [['vendor/rixafy/ip-address']]);
+		}
 	}
 
 	public function loadConfiguration(): void
